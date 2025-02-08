eee3c0a96376983ac530ff5507d124e0ace3a50d
Eslam Amin <eslam.amin.610@gmail.com>
2025-02-08 17:47:27 +0200

    Create user on fly

--- startup/createAdmin.js
+++ startup/createAdmin.js
@@ -1,21 +1,37 @@
 require("colors");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user.model");
-const { ADMIN } = require("../utils/constants");
+const { ADMIN, USER } = require("../utils/constants");
 
 const createMainAdmin = asyncHandler(async () => {
   const admin = await User.findOne({ role: ADMIN });
+  const user = await User.findOne({ role: USER });
   if (!admin) {
     const admin = new User({
       firstName: "Main",
       lastName: "Admin",
-      email: process.env.MAIN_ADMIN_EMAIL, // admin@gmail.com
-      password: process.env.MAIN_ADMIN_PASSWORD, // admin123
-      role: ADMIN,
+      // email: process.env.MAIN_ADMIN_EMAIL, // admin@gmail.com
+      email: "user@gmail.com", // admin@gmail.com
+      // password: process.env.MAIN_ADMIN_PASSWORD, // admin123
+      password: "user123", // user123
+      // role: ADMIN,
+      role: USER,
       isVerified: true
     });
-    console.log("=== Super admin created successfully ===".green);
+    console.log("=== Admin created successfully ===".green);
     await admin.save();
+  }
+  if (!user) {
+    const user = new User({
+      firstName: "Main",
+      lastName: "User",
+      email: "user@gmail.com",
+      password: "user123",
+      role: USER,
+      isVerified: true
+    });
+    console.log("=== User created successfully ===".green);
+    await user.save();
   }
 });
 
